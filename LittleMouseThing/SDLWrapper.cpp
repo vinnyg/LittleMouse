f64375536b81c0a3d83592a35915c636d15ec378
Vincent <vinnygiang@outlook.ac.uk>
2015-01-27 20:17:06 +0000

    Updated SDLWrapper::CreateWindow and added code to main.cpp for testing.

--- LittleMouseThing/SDLWrapper.cpp
+++ LittleMouseThing/SDLWrapper.cpp
@@ -9,10 +9,12 @@ SDLWrapper::SDLWrapper()
 SDLWrapper::~SDLWrapper()
 {
 	std::map<std::string, SDLWindow*>::iterator it = m_windowMap.begin();
-	while (it++ != m_windowMap.end())
+	while (it != m_windowMap.end())
 	{
 		//*it->second;
 		delete it->second;
+
+		++it;
 	}
 
 	SDL_QuitSubSystem(SDL_INIT_VIDEO);
@@ -25,13 +27,14 @@ SDLWrapper::~SDLWrapper()
 //Creates an SDL Window
 void SDLWrapper::CreateWindow(std::string windowRef, std::string title, int w, int h)
 {
+	if (!(SDL_WasInit(SDL_INIT_VIDEO) > 0))
+	{
+		SDL_Init(SDL_INIT_VIDEO);
+	}
+
 	if (SDL_WasInit(SDL_INIT_VIDEO) != 0)
 	{
 		m_windowMap.insert(std::make_pair(windowRef, new SDLWindow(title, w, h)));
-	}
-	else
-	{
-		SDL_Init(SDL_INIT_VIDEO);
 	}
 }
 
--- LittleMouseThing/main.cpp
+++ LittleMouseThing/main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char* args[])
 {
+	SDLWrapper* engine = new SDLWrapper();
+	engine->CreateWindow("someName", "Window", 640, 480);
 
+	delete engine;
 
 	return 0;
 }
