c003e6664acc5cdbc48715896e1740854ff89cbe
Vincent <vinnygiang@outlook.ac.uk>
2015-01-28 23:40:42 +0000

    Updated SetWindowSettings method. Explicit namespace referencing.

--- LittleMouseThing/SDLWindow.cpp
+++ LittleMouseThing/SDLWindow.cpp
@@ -1,70 +1,70 @@
 #include "SDLWindow.h"
-namespace LM
+
+LM::SDLWindow::SDLWindow(std::string title, int w, int h)
 {
+	m_pWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
+}
 
-	SDLWindow::SDLWindow(std::string title, int w, int h)
+LM::SDLWindow::~SDLWindow()
+{
+	SDL_DestroyWindow(m_pWindow);
+}
+
+void LM::SDLWindow::SetWindowTitle(std::string title)
+{
+	SDL_SetWindowTitle(m_pWindow, title.c_str());
+}
+
+void LM::SDLWindow::SetWindowSize(int w, int h)
+{
+	SDL_SetWindowSize(m_pWindow, w, h);
+}
+
+void LM::SDLWindow::SetWindowMode(SDLWindowMode mode)
+{
+	m_windowMode = mode;
+
+	switch (m_windowMode)
 	{
-		m_pWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
-	}
-
-	SDLWindow::~SDLWindow()
+	case SDLWindowMode::WM_WINDOWED:
 	{
-		SDL_DestroyWindow(m_pWindow);
+		SDL_SetWindowBordered(m_pWindow, SDL_FALSE);
+		SDL_SetWindowFullscreen(m_pWindow, SDL_FALSE);
 	}
-
-	void SDLWindow::SetWindowTitle(std::string title)
+	break;
+	case SDLWindowMode::WM_BORDERLESS:
 	{
-		if (SDL_GetWindowTitle(m_pWindow) != title.c_str())
-		{
-			SDL_SetWindowTitle(m_pWindow, title.c_str());
-		}
+		SDL_SetWindowBordered(m_pWindow, SDL_TRUE);
+		SDL_SetWindowFullscreen(m_pWindow, SDL_FALSE);
 	}
-
-	void SDLWindow::SetWindowSize(int w, int h)
+	break;
+	case SDLWindowMode::WM_FULLSCREEN:
 	{
-		int currentW, currentH;
-		SDL_GetWindowSize(m_pWindow, &currentW, &currentH);
-		if ((w != currentW) || (h != currentH))
-		{
-			SDL_SetWindowSize(m_pWindow, w, h);
-		}
+		SDL_SetWindowBordered(m_pWindow, SDL_FALSE);
+		SDL_SetWindowFullscreen(m_pWindow, SDL_TRUE);
 	}
-
-	void SDLWindow::SetWindowMode(SDLWindowMode mode)
-	{
-		if (m_windowMode != mode)
-		{
-			m_windowMode = mode;
-
-			switch (m_windowMode)
-			{
-			case SDLWindowMode::WM_WINDOWED:
-			{
-				SDL_SetWindowBordered(m_pWindow, SDL_FALSE);
-				SDL_SetWindowFullscreen(m_pWindow, SDL_FALSE);
-			}
-			break;
-			case SDLWindowMode::WM_BORDERLESS:
-			{
-				SDL_SetWindowBordered(m_pWindow, SDL_TRUE);
-				SDL_SetWindowFullscreen(m_pWindow, SDL_FALSE);
-			}
-			break;
-			case SDLWindowMode::WM_FULLSCREEN:
-			{
-				SDL_SetWindowBordered(m_pWindow, SDL_FALSE);
-				SDL_SetWindowFullscreen(m_pWindow, SDL_TRUE);
-			}
-			break;
-			}
-		}
+	break;
 	}
+}
 
-	//Change and apply window settings at once.
-	void SDLWindow::SetWindowSettings(std::string title, int w, int h, SDLWindowMode mode)
+//Change and apply window settings at once.
+void LM::SDLWindow::SetWindowSettings(std::string title, int w, int h, SDLWindowMode mode)
+{
+	//We do these checks out here to reduce function calls.
+	if (SDL_GetWindowTitle(m_pWindow) != title.c_str())
 	{
 		SetWindowTitle(title);
+	}
+
+	int currentW, currentH;
+	SDL_GetWindowSize(m_pWindow, &currentW, &currentH);
+	if ((w != currentW) || (h != currentH))
+	{
 		SetWindowSize(w, h);
+	}
+
+	if (m_windowMode != mode)
+	{
 		SetWindowMode(mode);
 	}
 }
