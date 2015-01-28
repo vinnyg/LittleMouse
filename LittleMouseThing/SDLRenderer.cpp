7328463a9738303c05fe8e3cd19a251f4a20afb5
Vincent <vinnygiang@outlook.ac.uk>
2015-01-28 19:54:43 +0000

    Updated renderer signatures.

---
+++ LittleMouseThing/SDLRenderer.cpp
@@ -0,0 +1,45 @@
+#include "SDLRenderer.h"
+
+SDLRenderer::SDLRenderer(SDL_Window* window)
+{
+	m_pRenderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+}
+
+SDLRenderer::~SDLRenderer()
+{
+	if (m_pRenderer != nullptr)
+	{
+		SDL_DestroyRenderer(m_pRenderer);
+	}
+}
+
+//Add a simple texture to the draw queue.
+void SDLRenderer::DrawTexture(SDL_Texture* tex, float src_x, float src_y, float src_w, float src_h, float dst_x, float dst_y, float dst_w, float dst_h)
+{
+	SDL_Rect src, dst;
+	src.x = src_x;
+	src.y = src_y;
+	src.w = src_w;
+	src.h = src_h;
+	dst.x = dst_x;
+	dst.y = dst_y;
+
+	SDL_RenderCopy(m_pRenderer, tex, &src, &dst);
+	//SDL_RenderCopyEx(m_pRenderer, tex, NULL, &dst, 0, NULL, SDL_FLIP_NONE);
+}
+
+//Add a texture to the draw queue with additional parameters.
+void SDLRenderer::DrawTexture(SDL_Texture* tex, float src_x, float src_y, float src_w, float src_h, float dst_x, float dst_y, float dst_w, float dst_h, float offset_x, float offset_y, double angle)
+{
+
+}
+
+void SDLRenderer::Render()
+{
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+}
