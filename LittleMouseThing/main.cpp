5af4146c8972288f602e90491f562853250d7bfa
Vincent <vinnygiang@outlook.ac.uk>
2015-01-29 19:11:18 +0000

    Modified for testing.

--- LittleMouseThing/main.cpp
+++ LittleMouseThing/main.cpp
@@ -1,12 +1,18 @@
-#include "SDLWindowComponentInterface.h"
-
+//#include "SDLWindowComponentInterface.h"
+#include "LMSDLWindow.h"
+#include "LMSDLRenderer.h"
 
 int main(int argc, char* args[])
 {
-	LM::SDLWindowComponentInterface* video = new LM::SDLWindowComponentInterface();
-	video->CreateWindow("someName", "Window", 640, 480);
+	//LM::SDLWindowComponentInterface* video = new LM::SDLWindowComponentInterface();
+	LM::LMSDLWindow* window = new LM::LMSDLWindow("Project", 640, 480);
+	//video->CreateWindow("someName", "Window", 640, 480);
 
-	delete video;
+	LM::LMSDLRenderer* render = new LM::LMSDLRenderer(window->GetWindow(), -1, 0);
+
+	delete render;
+	delete window;
+	//delete video;
 
 	return 0;
 }
