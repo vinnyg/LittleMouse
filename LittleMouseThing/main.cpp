402bf9f8a18da66cc57315152e111830048fb15c
vinnyg <vinnygiang@outlook.com>
2015-02-12 01:48:33 +0000

    Additional testing code.

--- LittleMouseThing/main.cpp
+++ LittleMouseThing/main.cpp
@@ -2,16 +2,25 @@
 #include "SDLWindow.h"
 #include "SDLRenderer.h"
 #include "SDLTexture.h"
-#include "SDLSurface.h"
-#include "SDLRenderFlipEnum.h"
+#include "SDLExtensionConfig.h"
+
 #include "SDLImage.h"
+
+#include "SDLRenderFlipEnum.h"
+//#include "Primitive.h"
+#include "PrimPixel.h"
+#include "SDLSurface.h"
+
+
 
 int main(int argc, char* args[])
 {
 	//LM::SDLWindowComponentInterface* video = new LM::SDLWindowComponentInterface();
 
 	//Testing multiple windows.
+#ifdef INCLUDE_SDL_IMAGE
 	LM::SDLImage img(IMG_INIT_PNG);
+#endif
 	LM::SDLWindow window("Project", 640, 480);
 	LM::SDLWindow window2("Project", 320, 240);
 	window.SetMode(LM::SDLWindowMode::kBorderless);
@@ -22,9 +31,7 @@ int main(int argc, char* args[])
 	//LM::SDLSurface sur("/Assets/Testing/retro_block_exclamation.bmp");
 #ifndef INCLUDE_SDL_IMAGE
 	LM::SDLSurface sur("C:\\LittleMouse\\LittleMouseThing\\Assets\\Testing\\retro_block_exclamation.bmp");
-#endif
-
-#ifdef INCLUDE_SDL_IMAGE
+#else
 	LM::SDLSurface sur("C:\\LittleMouse\\LittleMouseThing\\Assets\\Testing\\retro_block_exclamation.png");
 #endif
 
@@ -35,16 +42,26 @@ int main(int argc, char* args[])
 		0, 0, 0, 255
 	};
 
+	LM::ColorRGBA white =
+	{
+		255, 255, 255, 255
+	};
+
 	LM::Rect src(0, 0, 128, 128);
 	LM::Rect dst(0, 0, 256, 256);
 	LM::Point2 pt(0, 0);
 
+	LM::Point2 pt2(300, 300);
+	LM::PrimPixel pix(&render, pt2, white);
+
 	//sur.Blit(src, window.GetSurface(), dst);
 
-	//render.SetDrawColor(c);
+	render.SetDrawColor(c);
 	render.Clear();
 
 	tex.CopyToRenderer(&render, src, dst, pt, 0.0, LM::SDLRenderFlipEnum::kNoFlip);
+	
+	pix.Draw();
 
 	render.Present();
 
