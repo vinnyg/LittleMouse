2596187c2b4abc7864d5b51fc9901e4228b6c793
vinnyg <vinnygiang@outlook.com>
2015-02-13 17:51:33 +0000

    Kayboard class created.

---
+++ LittleMouseThing/Keyboard.cpp
@@ -0,0 +1,14 @@
+#include "Keyboard.h"
+
+namespace LM
+{
+	Keyboard::Keyboard()
+	{
+	}
+
+
+	Keyboard::~Keyboard()
+	{
+	}
+
+}
---
+++ LittleMouseThing/Keyboard.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <map>
+
+namespace LM
+{
+	class Keyboard
+	{
+	private:
+		std::map<int, bool> m_state;
+	public:
+		Keyboard();
+		~Keyboard();
+		bool IsKeyHeld();
+		bool IsKeyHit();
+	};
+}
