6fd33ca4c269fd365fd1b5a437b4089a86f65307
vinnyg <vinnygiang@outlook.com>
2015-02-19 23:47:26 +0000

    Finished method implementations. Removed overloaded RenderToSurface method. m_font member is now a shared pointer. Uint16 m_unicode member added to handle unicode characters. Overloaded constructor now takes a CharEncoding parameter. If Unicode encoding is chosen, encoding method will default to LATIN1. Additional constructor which takes Uint16 instead of std::string. Encoding method defaults to Unicode. Overloaded SetText method to take Uint16 for unicode text. Overloaded GetText to return unicode text. GetFont now returns a pointer to the SDLTTFont.

--- LittleMouseThing/TTFText.cpp
+++ LittleMouseThing/TTFText.cpp
@@ -1,11 +1,141 @@
 #include "TTFText.h"
 
-
-TTFText::TTFText()
+namespace LM
 {
+	TTFText::TTFText(std::string text, SDLTTFont* font, CharEncoding encoding) : m_text(text), m_font(std::make_shared<SDLTTFont>(font)), m_unicode(0), m_encoding(encoding != CharEncoding::ENC_UNICODE ? encoding : CharEncoding::ENC_LATIN1)
+	{
+	}
+
+	TTFText::TTFText(Uint16 text, SDLTTFont* font) : m_unicode(text), m_font(std::make_shared<SDLTTFont>(font)), m_text(""), m_encoding(CharEncoding::ENC_UNICODE)
+	{
+	}
+
+	TTFText::~TTFText()
+	{
+	}
+
+	SDLSurface TTFText::RenderToSurface(RenderMode mode, ColorRGBA fg, ColorRGBA bg)
+	{
+		SDL_Color c;
+		c.r = fg.r;
+		c.g = fg.g;
+		c.b = fg.b;
+		c.a = fg.a;
+
+		SDL_Color bc;
+		bc.r = bg.r;
+		bc.g = bg.g;
+		bc.b = bg.b;
+		bc.a = bg.a;
+
+		switch (m_encoding)
+		{
+		case CharEncoding::ENC_LATIN1:
+			switch (mode)
+			{
+			case RenderMode::RenderSolid:
+				TTF_RenderText_Solid(m_font->Get(), m_text.c_str(), c);
+				break;
+			case RenderMode::RenderShaded:
+				TTF_RenderText_Shaded(m_font->Get(), m_text.c_str(), c, bc);
+				break;
+			case RenderMode::RenderBlended:
+				TTF_RenderText_Blended(m_font->Get(), m_text.c_str(), c);
+				break;
+			}
+			break;
+		case CharEncoding::ENC_UNICODE:
+			switch (mode)
+			{
+			case RenderMode::RenderSolid:
+				TTF_RenderUNICODE_Solid(m_font->Get(), &m_unicode, c);
+				break;
+			case RenderMode::RenderShaded:
+				TTF_RenderUNICODE_Shaded(m_font->Get(), &m_unicode, c, bc);
+				break;
+			case RenderMode::RenderBlended:
+				TTF_RenderUNICODE_Blended(m_font->Get(), &m_unicode, c);
+				break;
+			}
+			break;
+		case CharEncoding::ENC_UTF8:
+			switch (mode)
+			{
+			case RenderMode::RenderSolid:
+				TTF_RenderUTF8_Solid(m_font->Get(), m_text.c_str(), c);
+				break;
+			case RenderMode::RenderShaded:
+				TTF_RenderUTF8_Shaded(m_font->Get(), m_text.c_str(), c, bc);
+				break;
+			case RenderMode::RenderBlended:
+				TTF_RenderUTF8_Blended(m_font->Get(), m_text.c_str(), c);
+				break;
+			}
+			break;
+		}
+	}
+
+	void TTFText::SetText(std::string text)
+	{
+		if (m_encoding != CharEncoding::ENC_UNICODE)
+		{
+			m_text = text;
+			m_unicode = 0;
+		}
+	}
+
+	void TTFText::SetText(Uint16 text)
+	{
+		if (m_encoding == CharEncoding::ENC_UNICODE)
+		{
+			m_unicode = text;
+			m_text.clear();
+		}
+	}
+
+	void TTFText::SetFont(SDLTTFont* font)
+	{
+		m_font = std::make_shared<SDLTTFont>(font);
+	}
+
+	int TTFText::GetTextSize(Rect* dimensions) const
+	{
+		int* w;
+		int* h;
+		int res;
+		switch (m_encoding)
+		{
+		case CharEncoding::ENC_LATIN1:
+			res = TTF_SizeText(m_font->Get(), m_text.c_str(), w, h);
+			break;
+		case CharEncoding::ENC_UNICODE:
+			res = TTF_SizeUNICODE(m_font->Get(), &m_unicode, w, h);
+			break;
+		case CharEncoding::ENC_UTF8:
+			res = TTF_SizeUTF8(m_font->Get(), m_text.c_str(), w, h);
+			break;
+		}
+
+		dimensions->SetWidth(*w);
+		dimensions->SetHeight(*h);
+		return res;
+	}
+
+	void TTFText::GetText(std::string* text) const
+	{
+		if (m_encoding != CharEncoding::ENC_UNICODE)
+			*text = m_text;
+	}
+
+	void TTFText::GetText(Uint16* text) const
+	{
+		if (m_encoding == CharEncoding::ENC_UNICODE)
+			*text = m_unicode;
+	}
+
+	SDLTTFont* TTFText::GetFont() const
+	{
+		return m_font.get();
+	}
 }
 
-
-TTFText::~TTFText()
-{
-}
--- LittleMouseThing/TTFText.h
+++ LittleMouseThing/TTFText.h
@@ -1,8 +1,32 @@
 #pragma once
-class TTFText
-{
-public:
-	TTFText();
-	~TTFText();
-};
+#include <memory>
+#include <string>
+#include "CharEncodingEnum.h"
+#include "SDLSurface.h"
+#include "Geometry.h"
+#include "SDLTTFont.h"
+#include "TextRenderModeEnum.h"
 
+namespace LM
+{
+	class TTFText
+	{
+	private:
+		std::string m_text;
+		std::shared_ptr<SDLTTFont> m_font;
+		Uint16 m_unicode;
+		CharEncoding m_encoding;
+	public:
+		TTFText(std::string text, SDLTTFont* font, CharEncoding encoding);
+		TTFText(Uint16 text, SDLTTFont* font);
+		~TTFText();
+		SDLSurface RenderToSurface(RenderMode mode, ColorRGBA fg, ColorRGBA bg);
+		void SetText(std::string text);
+		void SetText(Uint16 text);
+		void SetFont(SDLTTFont* font);
+		int GetTextSize(Rect* dimensions) const;
+		void GetText(std::string* text) const;
+		void GetText(Uint16* text) const;
+		SDLTTFont* GetFont() const;
+	};
+}
