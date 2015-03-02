3adf0535202fa895a4947c60aca42e6695861a07
vinnyg <vinnygiang@outlook.com>
2015-03-02 17:24:09 +0000

    Point2 has now been converted to a class template. .cpp file is now redundant.

--- LittleMouseThing/Point2.cpp
+++ LittleMouseThing/Point2.cpp
@@ -1,23 +1,22 @@
 #include "Point2.h"
 
+/*
 namespace LM
 {
 	Point2::Point2() : x(0), y(0)
 	{
 	}
 
-	Point2::Point2(int _x, int _y) : x(_x), y(_y)
-	{
-	}
+	template<class T>
+	Point2::Point2(T _x, T _y) : x(_x), y(_y) {}
 
-	Point2::Point2(Point2 const &point) : x(point.x), y(point.y)
-	{
-	}
+	template<class T>
+	Point2::Point2(Point2 const &point) : x(point.x), y(point.y) {}
 
-	Point2::Point2(SDL_Point const &point) : x(point.x), y(point.y)
-	{
-	}
+	template<class T>
+	Point2::Point2(SDL_Point const &point) : x(point.x), y(point.y) {}
 
+	template<class T>
 	Point2 Point2::operator=(Point2 const rhs)
 	{
 		this->x = rhs.x;
@@ -25,23 +24,28 @@ namespace LM
 		return *this;
 	}
 
+	template<class T>
 	Point2 Point2::operator+(Point2 const rhs)
 	{
 		return Point2(x + rhs.x, y + rhs.y);
 	}
 
+	template<class T>
 	Point2 Point2::operator-(Point2 const rhs)
 	{
 		return Point2(x - rhs.x, y - rhs.y);
 	}
 
+	template<class T>
 	Point2 Point2::operator*(int const rhs)
 	{
 		return Point2(x * rhs, y * rhs);
 	}
 
+	template<class T>
 	Point2 Point2::operator/(int const rhs)
 	{
 		return Point2(x / rhs, y / rhs);
 	}
 }
+*/
--- LittleMouseThing/Point2.h
+++ LittleMouseThing/Point2.h
@@ -4,7 +4,7 @@
 
 namespace LM
 {
-	class Point2 : public SDL_Point
+	/*class Point2 : public SDL_Point
 	{
 	private:
 		int x;
@@ -25,6 +25,36 @@ namespace LM
 		Point2 operator*(int const rhs);
 		Point2 operator/(int const rhs);
 		Point2 operator=(Point2 const rhs);
-	};
+	};*/
 
+	template <class type>
+	class Point2 : public SDL_Point
+	{
+	private:
+		type x, y;
+	public:
+		Point2(type _x, type _y) : x(_x), y(_y) {}
+		Point2(SDL_Point const &point) : x(point.x), y(point.y) {}
+		Point2(Point2 const &point) : x(point.x), y(point.y) {}
+
+		void SetX(type _x) { x = _x; }
+		void SetY(type _y) { y = _y; }
+		type GetX() const { return x; }
+		type GetY() const { return y; }
+
+		Point2<type> operator+(Point2<type> const rhs) { return Point2<type>(x + rhs.x, y + rhs.y); }
+		Point2<type> operator-(Point2<type> const rhs) { return Point2<type>(x - rhs.x, y - rhs.y); }
+
+		template <typename operandType>
+		Point2<type> operator*(operandType const rhs) { return Point2<type>(x * rhs, y * rhs); }
+		template <typename operandType>
+		Point2<type> operator/(operandType const rhs) { return Point2<type>(x / rhs, y / rhs) }
+		template <typename operandType>
+		Point2<type> operator=(Point2<operandType> const rhs)
+		{
+			this->x = rhs.GetX();
+			this->y = rhs.GetY();
+			return *this;
+		}
+	};
 }
