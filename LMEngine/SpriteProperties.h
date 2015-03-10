2371d23bb45c968356c75721bedb99a75aa5870e
vinnyg <vinnygiang@outlook.com>
2015-03-10 18:13:49 +0000

    srcRegion was previously not in the initializer list.

--- LMEngine/SpriteProperties.h
+++ LMEngine/SpriteProperties.h
@@ -12,6 +12,6 @@ namespace LiME
 
 		SpriteProperties() = delete;
 		SpriteProperties(LM::Rect<int> srcRegion) : SpriteProperties(srcRegion, 0.0f, 255, { 0, 0 }, { 1.0f, 1.0f }) {}
-		SpriteProperties(LM::Rect<int> srcRegion, float angle, Uint8 alpha, LM::Point2<int> origin, LM::Point2<float> scale) : angle(angle), alpha(alpha), origin(origin), scale(scale) {}
+		SpriteProperties(LM::Rect<int> srcRegion, float angle, Uint8 alpha, LM::Point2<int> origin, LM::Point2<float> scale) : srcRegion(srcRegion), angle(angle), alpha(alpha), origin(origin), scale(scale) {}
 	};
 }
