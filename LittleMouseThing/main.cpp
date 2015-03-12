7eb9e2188e0884a94e02d811c637f853699a5f34
vinnyg <vinnygiang@outlook.com>
2015-03-12 18:41:12 +0000

    Code for engine testing.

--- LittleMouseThing/main.cpp
+++ LittleMouseThing/main.cpp
@@ -1,14 +1,19 @@
 // #include "SDLWindowComponentInterface.h"
 /*#include "SDLExtensionConfig.h"
 #include "LMWrapper.h"
-#include "SDLImage.h"
 #include "SDLTTFont.h"
 #include "TTFText.h"*/
 #include "LMWrapper.h"
 
+#include "SDLImage.h"
+
+#include "LiME/TextureLoader.h"
+#include "LiME/SpriteManager.h"
+#include "LiME/Sprite.h"
+
 int main(int argc, char* args[])
 {
-	/*SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING);
 	//Testing multiple windows.
 #ifdef INCLUDE_SDL_IMAGE
 	LM::SDLImage img(IMG_INIT_PNG);
@@ -20,7 +25,7 @@ int main(int argc, char* args[])
 
 	LM::SDLRenderer render(window.Get(), -1, 0);
 	LM::SDLRenderer render2(window2.Get(), -1, 0);
-
+	/*
 #ifndef INCLUDE_SDL_IMAGE
 	LM::SDLSurface sur("C:\\LittleMouse\\LittleMouseThing\\Assets\\Testing\\retro_block_exclamation.bmp");
 #else
@@ -33,19 +38,10 @@ int main(int argc, char* args[])
 
 	LM::SDLTTFont fon("C:/windows/fonts/arial.ttf", 24);
 	LM::TTFText text1("this is text!!465ty", &fon, LM::CharEncoding::ENC_LATIN1);
-	LM::Keyboard keyboard;
-	LM::Mouse mouse;
-
-	bool isRunning = true;
-
-	LM::ColorRGBA ra =
-	{
-		255, 0, 0, 128
-	};
 
 	LM::Rect<int> src(0, 0, 128, 128);
 	LM::Rect<int> dst(0, 0, 256, 256);
-	LM::Rect<int> rct(100, 100, 300, 310);
+	
 	LM::Point2<double> d_pt(0.0, 0.0);
 	LM::Point2<double> d_ptu = d_pt;
 	LM::Point2<int> pt(20, 20);
@@ -54,7 +50,6 @@ int main(int argc, char* args[])
 	LM::Point2<int> pt2(300, 300);
 	LM::PixelPrimitive<float> pix(&render, pt2, LM::Color::WHITE);
 	LM::LinePrimitive<int> lin(&render, pt, pt2, LM::Color::WHITE);
-	LM::RectPrimitive<int> rec(&render, rct, true, ra);
 
 	LM::SDLSurface texSurface = text1.RenderToSurface(LM::RenderMode::RenderShaded, LM::Color::WHITE, LM::Color::BLACK);
 	LM::SDLTexture text1rend(&render, &texSurface);
@@ -62,7 +57,32 @@ int main(int argc, char* args[])
 	text1rend.Query(&t_w, &t_h);
 	LM::Rect<int> dstText(200, 250, t_w, t_h);
 
-	//sur.Blit(src, window.GetSurface(), dst);
+	//sur.Blit(src, window.GetSurface(), dst);*/
+
+	//TTF_Quit();
+
+	LiME::TextureLoader tLoader(&render);
+	LiME::SpriteManager manager;
+
+	LM::Keyboard keyboard;
+	LM::Mouse mouse;
+
+	LM::ColorRGBA ra =
+	{
+		255, 0, 0, 128
+	};
+
+	LM::Rect<int> rct(100, 100, 300, 310);
+	LM::RectPrimitive<int> rec(&render, rct, true, ra);
+
+	bool isRunning = true;
+	//std::shared_ptr<LM::SDLSurface> s(new LM::SDLSurface("./Assets/Testing/retro_block_exclamation.png"));
+	//std::shared_ptr<LM::SDLTexture> t(new LM::SDLTexture(&render, s.get()));
+	std::shared_ptr<LM::SDLTexture> t = tLoader.LoadTexture("./Assets/Testing/retro_block_exclamation.png");
+	std::shared_ptr<LiME::Sprite> spr = manager.CreateSprite(t);
+	spr->SetPosition(LM::Point2<float>(20.0f, 20.0f));
+	spr->SetScale(LM::Point2<float>(2.0f, 2.0f));
+	//LM::SDLTexture tex = ;
 
 	while (isRunning)
 	{
@@ -72,20 +92,20 @@ int main(int argc, char* args[])
 			switch (e.type)
 			{
 			case SDL_QUIT:
-				{
+			{
 
-				} break;
+			} break;
 
 			case SDL_KEYDOWN:
-				{
+			{
 
-				} break;
-				
+			} break;
+
 			}
 		}
-		
-		if (keyboard.KeyIsHeld(LM::Key::Key_ESCAPE)/* && keyboard.IsKeyHeld(LM::Key::Key_a) *//*&& keyboard.IsKeyHeld(LM::Key::Key_w))*/
-		/*{
+
+		if (keyboard.KeyIsHeld(LM::Key::Key_ESCAPE))/* && keyboard.IsKeyHeld(LM::Key::Key_a) *//*&& keyboard.IsKeyHeld(LM::Key::Key_w))*/
+		{
 			isRunning = !isRunning;
 		}
 
@@ -103,26 +123,23 @@ int main(int argc, char* args[])
 		render.SetDrawBlendMode(SDL_BLENDMODE_BLEND);
 		render.Clear();
 
-		render2.SetDrawColor(LM::Color::BLACK);
+		/*render2.SetDrawColor(LM::Color::BLACK);
 		render2.SetDrawBlendMode(SDL_BLENDMODE_BLEND);
-		render2.Clear();
+		render2.Clear();*/
 
-		text1rend.CopyToRenderer(dstText);
+		/*text1rend.CopyToRenderer(dstText);
 		tex.CopyToRenderer(src, dst, pt, 0.0, LM::SDLRenderFlipEnum::kNoFlip);
 		tex2.CopyToRenderer(src, dst, pt, 0.0, LM::SDLRenderFlipEnum::kNoFlip);
 		pix.SetDrawColor(LM::Color::WHITE);
 		lin.SetDrawColor(LM::Color::WHITE);
 		pix.Draw();
-		lin.Draw();
+		lin.Draw();*/
 		rec.Draw();
+		manager.Draw();
 
 		render.Present();
 		render2.Present();
-	}*/
-
-	//TTF_Quit();
-
-
+	}
 
 	return 0;
 }
