5ce8b69f724c6a636fde1220190f7e678b0b8fb5
vinnyg <vinnygiang@outlook.com>
2015-03-13 12:32:45 +0000

    GetState method refactored to GetState and GetPosition.

--- LittleMouseThing/Mouse.cpp
+++ LittleMouseThing/Mouse.cpp
@@ -2,19 +2,20 @@
 
 namespace LM
 {
-	Mouse::Mouse()
-	{
+	Mouse::Mouse() : position(0, 0) {}
 
+	Mouse::~Mouse() {}
+
+	Uint32 Mouse::GetState()
+	{
+		return SDL_GetMouseState(nullptr, nullptr);
 	}
 
-	Mouse::~Mouse()
+	Point2<int> Mouse::GetPosition()
 	{
-
-	}
-
-	Uint32 Mouse::GetState(int* x, int*y)
-	{
-		return SDL_GetMouseState(x, y);
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+		return Point2<int>(x, y);
 	}
 
 	Uint32 Mouse::GetRelativeState(int* x, int*y)
@@ -34,19 +35,19 @@ namespace LM
 		switch (mb)
 		{
 		case Button::MB_Left:
-			isPressed = ((GetState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0);
+			isPressed = ((GetState() & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0);
 			break;
 		case Button::MB_Middle:
-			isPressed = ((GetState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0);
+			isPressed = ((GetState() & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0);
 			break;
 		case Button::MB_Right:
-			isPressed = ((GetState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0);
+			isPressed = ((GetState() & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0);
 			break;
 		case Button::MB_X1:
-			isPressed = ((GetState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_X1)) != 0);
+			isPressed = ((GetState() & SDL_BUTTON(SDL_BUTTON_X1)) != 0);
 			break;
 		case Button::MB_X2:
-			isPressed = ((GetState(nullptr, nullptr) & SDL_BUTTON(SDL_BUTTON_X2)) != 0);
+			isPressed = ((GetState() & SDL_BUTTON(SDL_BUTTON_X2)) != 0);
 			break;
 		default:
 			isPressed = false;
