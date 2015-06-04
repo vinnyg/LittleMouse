ef1c02a16153e1b8faaf33897fca1b7321eb5327
vinnyg <vinnygiang@outlook.com>
2015-06-04 12:16:29 +0100

    Data members now public; removed redundant getter/setter mutators.

--- LittleMouseThing/Point2.h
+++ LittleMouseThing/Point2.h
@@ -7,19 +7,19 @@ namespace LM
 	template <class type>
 	class Point2 : public SDL_Point
 	{
-	private:
-		type x, y;
 	public:
+		type x, y;
+
 		Point2() = delete;
 		Point2(type _x, type _y) : x(_x), y(_y) {}
 		//Point2(SDL_Point const &point) : x(point.x), y(point.y) {}
 		Point2(Point2<type> const &point) : x(point.x), y(point.y) {}
 		~Point2() {}
 
-		type GetX() const { return x; }
+		/*type GetX() const { return x; }
 		type GetY() const { return y; }
 		void SetX(type _x) { x = _x; }
-		void SetY(type _y) { y = _y; }
+		void SetY(type _y) { y = _y; }*/
 
 		SDL_Point ToSDLPoint() const
 		{
